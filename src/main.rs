4013a2db1d172ceaa6d564ea91d72fe91ef44bb6
BuildTools <unconfigured@null.spigotmc.org>
2021-03-29 19:45:26 +0200

    Create and print first minimal chess board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod game;
+
 fn main() {
     println!("This project is meant to create a simple chess game, to pratice with Rust.");
+    
+    let mut new_board = game::Board{cases:[[None; 8]; 8]};
+    new_board.init_pawns();
+    new_board.print();
 }
+
