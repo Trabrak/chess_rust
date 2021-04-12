2232d224973515cf24237079b2764310c3f8323a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-12 23:39:49 +0200

    First unit tests in util + All pieces types + update start board

--- src/game.rs
+++ src/game.rs
@@ -9,8 +9,12 @@ mod piece
     #[derive(Copy, Clone)] 
     pub enum Type
     {
-        PAWN,
-        KING
+        Pawn,
+        King,
+        Queen,
+        Rook,
+        Bishop,
+        Knight
     }
 
     /// Struct to represent Pawn in chess game
@@ -18,16 +22,22 @@ mod piece
     pub struct Piece
     {
         is_white:bool,
-        r#type:Type
+        p_type:Type
     }
 
     impl Piece 
     {
-        pub fn how_can_i_move()
+        pub fn description(&self)
         {
-            println!("I am a pawn, I can move forward to the unoccupied square immediately \
-            in front of me on the same file, or on my first move I can advance two squares \
-            along the same file ");
+            match self.p_type
+            {
+                Pawn => {println!("I am a pawn, I can move forward to the unoccupied square immediately in front of me on the same file, or on my first move I can advance two squares along the same file ");}
+                King => { println!("I move one square in any direction. I also have a special move called castling that involves also moving a rook.");}
+                Queen => {println!("I combine the power of a rook and bishop and can move any number of squares along a rank, file, or diagonal, but cannot leap over other pieces.");}
+                Rook => {println!(" Ican move any number of squares along a rank or file, but cannot leap over other pieces. Along with the king, I am involved during the king's castling move.");}
+                Bishop => {println!("I can move any number of squares diagonally, but cannot leap over other pieces.");}
+                Knight => {println!("I move to any of the closest square that are not on the same rank, file, or diagonal. (Thus the move forms an \"L\"-shape: two squares vertically and one square horizontally, or two squares horizontally and one square vertically.) The knight is the only piece that can leap over other pieces.");}
+            }            
         }
     
         pub fn print(&self)
@@ -38,9 +48,9 @@ mod piece
             }
         }
 
-        pub fn new_pawn() -> Piece
+        pub fn new_piece(white:bool, piece_type:Type) -> Piece
         {
-            Piece{is_white:true, r#type:Type::PAWN}
+            Piece{is_white:true, p_type:piece_type}
         }
     }
 }
@@ -54,24 +64,49 @@ struct Board
 
 impl Board
 {
-    /// Put 16 White & 16 Black pawn on empty board 
-    pub fn init_pawns(&mut self)
+    /// Put 16 White & 16 Black pieces on empty board 
+    pub fn init(&mut self)
     {
+        // Pawns
         for i_file in 0..8 //file in cases
         {
-            for i_rank in 0..2 //rank in file
+            for i_rank in 1..2 //rank in file
             {
-                self.cases[i_file][i_rank] = Some(piece::Piece::new_pawn());
+                self.cases[i_file][i_rank] = Some(piece::Piece::new_piece(true, piece::Type::Pawn));
             }
         }
+        for i_file in 0..8 //file in cases
+        {
+            for i_rank in 6..7 //rank in file
+            {
+                self.cases[i_file][i_rank] = Some(piece::Piece::new_piece(false, piece::Type::Pawn)); 
+            }
+        }
+        // Rooks
+        self.cases[0][0] = Some(piece::Piece::new_piece(true, piece::Type::Rook));
+        self.cases[7][0] = Some(piece::Piece::new_piece(true, piece::Type::Rook)); 
+        self.cases[0][7] = Some(piece::Piece::new_piece(false, piece::Type::Rook)); 
+        self.cases[7][7] = Some(piece::Piece::new_piece(false, piece::Type::Rook)); 
 
-        for i_file in 0..8 //file in cases
-        {
-            for i_rank in 6..8 //rank in file
-            {
-                self.cases[i_file][i_rank] = Some(piece::Piece::new_pawn()); 
-            }
-        }
+        // Knights
+        self.cases[1][0] = Some(piece::Piece::new_piece(true, piece::Type::Knight));
+        self.cases[6][0] = Some(piece::Piece::new_piece(true, piece::Type::Knight)); 
+        self.cases[1][7] = Some(piece::Piece::new_piece(false, piece::Type::Knight)); 
+        self.cases[6][7] = Some(piece::Piece::new_piece(false, piece::Type::Knight)); 
+
+        // Bishops
+        self.cases[2][0] = Some(piece::Piece::new_piece(true, piece::Type::Bishop));
+        self.cases[5][0] = Some(piece::Piece::new_piece(true, piece::Type::Bishop)); 
+        self.cases[2][7] = Some(piece::Piece::new_piece(false, piece::Type::Bishop)); 
+        self.cases[5][7] = Some(piece::Piece::new_piece(false, piece::Type::Bishop)); 
+
+        // Queens
+        self.cases[3][0] = Some(piece::Piece::new_piece(true, piece::Type::Bishop));
+        self.cases[3][7] = Some(piece::Piece::new_piece(true, piece::Type::Bishop)); 
+
+        // Kings
+        self.cases[4][0] = Some(piece::Piece::new_piece(true, piece::Type::Bishop));
+        self.cases[4][7] = Some(piece::Piece::new_piece(true, piece::Type::Bishop));
     }
 
     /// From a given position, tells you where you can move your pawn.
@@ -129,7 +164,7 @@ impl Game
         }
         else
         {
-            let array_entries = util::convert_board_pos_to_array_entry(&from);
+            let array_entries = util::convert_board_pos_to_array_entry(&from).unwrap();
             //println!("For {}, array entries are {} - {}", &from, array_entries[0], array_entries[1]);
             if self.board.cases[array_entries[0]][array_entries[1]].is_some()
             {
--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,21 @@
 // Converts board pos ([A-H][1-8]) to [0-7][0-7]
-pub fn convert_board_pos_to_array_entry(file_and_rank:&str) -> [usize; 2]
+pub fn convert_board_pos_to_array_entry(file_and_rank:&str) -> Option<[usize; 2]>
 {
+    if file_and_rank.len() != 2
+    {
+        return None;
+    }
     // no test here, we assume that the caller checked the format before !
     let to_upper = file_and_rank.to_uppercase();
     let file = to_upper.chars().nth(0).unwrap();
     let rank = to_upper.chars().nth(1).unwrap();
-    let mut file_entry:usize = 0;
+    let file_entry:usize;
     let rank_entry:usize = (rank.to_digit(10).unwrap() - 1) as usize ;
+
+    if rank_entry > 7
+    {
+        return None
+    }
     match file
     {
         'A' => {file_entry = 0;}
@@ -17,8 +26,100 @@ pub fn convert_board_pos_to_array_entry(file_and_rank:&str) -> [usize; 2]
         'F' => {file_entry = 5;}
         'G' => {file_entry = 6;}
         'H' => {file_entry = 7;}
-        _ => {println!("Hum... Have you checked the entry before calling this function?");}
+        _ => return None
     }
     
-    return [file_entry, rank_entry]
+    return Some([file_entry, rank_entry])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_all_entries() 
+    {
+        assert_eq!(convert_board_pos_to_array_entry("A1"), Some([0,0]));
+        assert_eq!(convert_board_pos_to_array_entry("A2"), Some([0,1]));
+        assert_eq!(convert_board_pos_to_array_entry("A3"), Some([0,2]));
+        assert_eq!(convert_board_pos_to_array_entry("A4"), Some([0,3]));
+        assert_eq!(convert_board_pos_to_array_entry("A5"), Some([0,4]));
+        assert_eq!(convert_board_pos_to_array_entry("A6"), Some([0,5]));
+        assert_eq!(convert_board_pos_to_array_entry("A7"), Some([0,6]));
+        assert_eq!(convert_board_pos_to_array_entry("A8"), Some([0,7]));
+        assert_eq!(convert_board_pos_to_array_entry("B1"), Some([1,0]));
+        assert_eq!(convert_board_pos_to_array_entry("B2"), Some([1,1]));
+        assert_eq!(convert_board_pos_to_array_entry("B3"), Some([1,2]));
+        assert_eq!(convert_board_pos_to_array_entry("B4"), Some([1,3]));
+        assert_eq!(convert_board_pos_to_array_entry("B5"), Some([1,4]));
+        assert_eq!(convert_board_pos_to_array_entry("B6"), Some([1,5]));
+        assert_eq!(convert_board_pos_to_array_entry("B7"), Some([1,6]));
+        assert_eq!(convert_board_pos_to_array_entry("B8"), Some([1,7]));
+        assert_eq!(convert_board_pos_to_array_entry("C1"), Some([2,0]));
+        assert_eq!(convert_board_pos_to_array_entry("C2"), Some([2,1]));
+        assert_eq!(convert_board_pos_to_array_entry("C3"), Some([2,2]));
+        assert_eq!(convert_board_pos_to_array_entry("C4"), Some([2,3]));
+        assert_eq!(convert_board_pos_to_array_entry("C5"), Some([2,4]));
+        assert_eq!(convert_board_pos_to_array_entry("C6"), Some([2,5]));
+        assert_eq!(convert_board_pos_to_array_entry("C7"), Some([2,6]));
+        assert_eq!(convert_board_pos_to_array_entry("C8"), Some([2,7]));
+        assert_eq!(convert_board_pos_to_array_entry("D1"), Some([3,0]));
+        assert_eq!(convert_board_pos_to_array_entry("D2"), Some([3,1]));
+        assert_eq!(convert_board_pos_to_array_entry("D3"), Some([3,2]));
+        assert_eq!(convert_board_pos_to_array_entry("D4"), Some([3,3]));
+        assert_eq!(convert_board_pos_to_array_entry("D5"), Some([3,4]));
+        assert_eq!(convert_board_pos_to_array_entry("D6"), Some([3,5]));
+        assert_eq!(convert_board_pos_to_array_entry("D7"), Some([3,6]));
+        assert_eq!(convert_board_pos_to_array_entry("D8"), Some([3,7]));
+        assert_eq!(convert_board_pos_to_array_entry("E1"), Some([4,0]));
+        assert_eq!(convert_board_pos_to_array_entry("E2"), Some([4,1]));
+        assert_eq!(convert_board_pos_to_array_entry("E3"), Some([4,2]));
+        assert_eq!(convert_board_pos_to_array_entry("E4"), Some([4,3]));
+        assert_eq!(convert_board_pos_to_array_entry("E5"), Some([4,4]));
+        assert_eq!(convert_board_pos_to_array_entry("E6"), Some([4,5]));
+        assert_eq!(convert_board_pos_to_array_entry("E7"), Some([4,6]));
+        assert_eq!(convert_board_pos_to_array_entry("E8"), Some([4,7]));
+        assert_eq!(convert_board_pos_to_array_entry("F1"), Some([5,0]));
+        assert_eq!(convert_board_pos_to_array_entry("F2"), Some([5,1]));
+        assert_eq!(convert_board_pos_to_array_entry("F3"), Some([5,2]));
+        assert_eq!(convert_board_pos_to_array_entry("F4"), Some([5,3]));
+        assert_eq!(convert_board_pos_to_array_entry("F5"), Some([5,4]));
+        assert_eq!(convert_board_pos_to_array_entry("F6"), Some([5,5]));
+        assert_eq!(convert_board_pos_to_array_entry("F7"), Some([5,6]));
+        assert_eq!(convert_board_pos_to_array_entry("F8"), Some([5,7]));
+        assert_eq!(convert_board_pos_to_array_entry("G1"), Some([6,0]));
+        assert_eq!(convert_board_pos_to_array_entry("G2"), Some([6,1]));
+        assert_eq!(convert_board_pos_to_array_entry("G3"), Some([6,2]));
+        assert_eq!(convert_board_pos_to_array_entry("G4"), Some([6,3]));
+        assert_eq!(convert_board_pos_to_array_entry("G5"), Some([6,4]));
+        assert_eq!(convert_board_pos_to_array_entry("G6"), Some([6,5]));
+        assert_eq!(convert_board_pos_to_array_entry("G7"), Some([6,6]));
+        assert_eq!(convert_board_pos_to_array_entry("G8"), Some([6,7]));
+        assert_eq!(convert_board_pos_to_array_entry("H1"), Some([7,0]));
+        assert_eq!(convert_board_pos_to_array_entry("H2"), Some([7,1]));
+        assert_eq!(convert_board_pos_to_array_entry("H3"), Some([7,2]));
+        assert_eq!(convert_board_pos_to_array_entry("H4"), Some([7,3]));
+        assert_eq!(convert_board_pos_to_array_entry("H5"), Some([7,4]));
+        assert_eq!(convert_board_pos_to_array_entry("H6"), Some([7,5]));
+        assert_eq!(convert_board_pos_to_array_entry("H7"), Some([7,6]));
+        assert_eq!(convert_board_pos_to_array_entry("H8"), Some([7,7]));
+    }
+
+    #[test]
+    fn test_too_long_entry()
+    {
+        assert_eq!(convert_board_pos_to_array_entry("Hello"), None);
+    }
+    
+    #[test]
+    fn test_not_on_board()
+    {
+        assert_eq!(convert_board_pos_to_array_entry("A9"), None);
+    }
+    
+    #[test]
+    fn test_empty()
+    {
+        assert_eq!(convert_board_pos_to_array_entry(""), None);
+    }
 }
